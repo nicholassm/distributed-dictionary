3d68114dbdf76dee9192d30aea97bc9694cc8d74
Nicholas Schultz-Møller <nicholassm@gmail.com>
2016-03-03 19:56:18 +0100

    Fix typos.

--- src/main/java/systems/speedy/distributed/dictionary/DistributedByteDictionary.java
+++ src/main/java/systems/speedy/distributed/dictionary/DistributedByteDictionary.java
@@ -5,13 +5,13 @@ package systems.speedy.distributed.dictionary;
 public interface DistributedByteDictionary {
 	/**
 	 * @param s The string to lookup a unique byte code for.
-	 * @return The byte representing the String s. If a unique byte cannot be returned for s then a Runtime Exception is thrown (this is a coding error).
+	 * @return The byte representing the String s. If a unique byte cannot be returned for s then a RuntimeException is thrown (this is a coding error).
 	 */
 	public byte lookup(String s);
 
 	/**
 	 * @param code The byte code for a String.
-	 * @return The String for the byte code. If a String is not stored for the byte code then a Runtime Exception is thrown (this is a coding error).
+	 * @return The String for the byte code. If a String is not stored for the byte code then a RuntimeException is thrown (this is a coding error).
 	 */
 	public String lookup(byte code);
 }
